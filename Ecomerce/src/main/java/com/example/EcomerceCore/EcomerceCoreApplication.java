8c2ffcab9fe788879d33643e9847b8f0f14fb58f
Apostolot <apostol516@gmail.com>
2019-07-12 18:44:06 +0200

    asda

--- Ecomerce/src/main/java/com/example/EcomerceCore/EcomerceCoreApplication.java
+++ Ecomerce/src/main/java/com/example/EcomerceCore/EcomerceCoreApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcomerceCoreApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcomerceCoreApplication.class, args);
 	}
